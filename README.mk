1b58b055dbbffd94e99f1bc76c866fcaaa0dc06d
nayib <nayib.abdala@gmail.com>
2023-03-22 06:17:02 -0500

    Add README.mk

---
+++ README.mk
@@ -0,0 +1,6 @@
+## dApp Example 1
+
+* A simple dApp with a front end in html css and vanilla JS
+* A backend with Solidity
+* Goerli used as a testing etherium network
+* IPFS as a protocol for file exchange on decentrilized network
